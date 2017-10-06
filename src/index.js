c7b0afe16e6b2d3c245cef386a1e56fcbb1b119e
plusCubed <pluscubed@users.noreply.github.com>
2017-10-05 21:35:47 -0700

    Add isPrerendering

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import ReactDOM from 'react-dom';
 
 export const render = (rootComponent, domElement) => {
-  if (
-    navigator.userAgent.match(/Node\.js/i) &&
-    window &&
-    window.reactSnapshotRender &&
-    window.ReactDOMServer
-  ) {
+  if (isPrerendering()) {
     domElement.innerHTML = window.ReactDOMServer.renderToString(rootComponent);
     window.reactSnapshotRender();
   } else {
     ReactDOM.render(rootComponent, domElement);
   }
+};
+
+export const isPrerendering = () => {
+  return navigator.userAgent.match(/Node\.js/i) &&
+    window &&
+    window.reactSnapshotRender &&
+    window.ReactDOMServer;
 };
