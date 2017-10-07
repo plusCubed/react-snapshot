791fcbc2d901cb0f11a807eaeeace706a3031e3e
plusCubed <pluscubed@users.noreply.github.com>
2017-10-06 22:28:03 -0700

    Add prerender

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import ReactDOM from 'react-dom';
 
 export const render = (rootComponent, domElement) => {
   if (isPrerendering()) {
-    domElement.innerHTML = window.ReactDOMServer.renderToString(rootComponent);
-    window.reactSnapshotRender();
+    prerender(rootComponent, domElement);
   } else {
     ReactDOM.render(rootComponent, domElement);
   }
+};
+
+export const prerender = (rootComponent, domElement) => {
+  domElement.innerHTML = window.ReactDOMServer.renderToString(rootComponent);
+  window.reactSnapshotRender();
 };
 
 export const isPrerendering = () => {
